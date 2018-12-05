b65f378d745cd2f5840fbaf30debae30e7441f08
Tim Paine <t.paine154@gmail.com>
2018-12-05 13:36:39 -0500

    updates

--- app.py
+++ app.py
@@ -4,41 +4,63 @@ import flask
 import functools
 import dash
 import pyEX as p
+import pyEXstudies as ps
 import json
 from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 from perspective_dash_component import PerspectiveDash
 
-# Default data
-symbols = p.symbols()
-default_data = p.chartDF('JPM', '6m').to_dict(orient='records')
 
+################################
+# Helpers to cache data lookup #
+@functools.lru_cache(100)
+def fetch_data(value):
+    return p.chartDF(value, '6m').to_dict(orient='records')
+
+
+@functools.lru_cache(100)
+def fetch_corr_data(value):
+    df = ps.peerCorrelation(value)
+    df.index.name = 'symbols'
+    return df.index.tolist(), df.reset_index().to_dict(orient='records')
+################################
+
+################
+# Default data #
+symbols = p.symbols()
+default_data = fetch_data('JPM')
+default_data2 = fetch_corr_data('JPM')
+default_data2cols, default_data2data = default_data2
+################
+
+################
 # dash
 server = flask.Flask(__name__)
 app = dash.Dash(__name__, server=server)
+################
 
+################
+# layout
 app.layout = html.Div(children=[
     html.H1(children='Perspective Demo', style={'textAlign': 'center'}),
     dcc.Dropdown(id='tickerinput', value='JPM', options=[{'label': s['symbol'] + ' - ' + s['name'], 'value': s['symbol']} for s in symbols]),
     PerspectiveDash(id='psp1', data=default_data, view='y_line', columns=['open', 'high', 'low', 'close']),
-    PerspectiveDash(id='psp2', data=default_data),
+    html.Div(children=[
+                        PerspectiveDash(id='psp2', data=default_data),
+                        PerspectiveDash(id='psp3', data=default_data2data, view='heatmap', columns=default_data2cols, rowpivots=['symbols'])],
+             style={'width': '100%', 'display': 'flex', 'flex-direction': 'row'}),
     html.Div(id='intermediate-value', style={'display': 'none'})
     ],
-    style={'height': '100%',
-           'width': '100%',
-           'display': 'flex',
-           'flex-direction': 'column'})
+    style={'height': '100%', 'width': '100%', 'display': 'flex', 'flex-direction': 'column'})
+################
 
 
-@functools.lru_cache(100)
-def fetch_data(value):
-    return p.chartDF(value, '6m')
-
-
+################
+# callbacks
 @app.callback(Output('intermediate-value', 'children'), [Input('tickerinput', 'value')])
 def fetch_new_data(value):
-    return fetch_data(value).to_json(orient='records')
+    return json.dumps(fetch_data(value))
 
 
 @app.callback(Output('psp1', 'data'), [Input('intermediate-value', 'children')])
@@ -50,6 +72,21 @@ def update_psp1(value):
 def update_psp2(value):
     return json.loads(value)
 
+
+# Data
+@app.callback(Output('psp3', 'data'), [Input('tickerinput', 'value')])
+def update_psp3data(value):
+    return fetch_corr_data(value)[1]
+
+
+# Columns
+@app.callback(Output('psp3', 'columns'), [Input('tickerinput', 'value')])
+def update_psp3cols(value):
+    return fetch_corr_data(value)[0]
+################
+
+################
+# Run
 if __name__ == "__main__":
     port = os.environ.get('PORT')
     # heroku
